02203f213eac5bbf5c53decbca05d6e6a0a80b2f
moshaosama <mohamedOSFekry@gmail.com>
2025-08-10 01:50:05 +0300

    feat: add Features module

---
+++ src/Features/Filteration/Components/FilterationInput.tsx
@@ -0,0 +1,27 @@
+import { Search } from "lucide-react";
+import { cn } from "../../../lib/utils";
+import { Content } from "../../../Style/GlobalStyle";
+import { useThemeContext } from "../../../Context/useTheme";
+
+const FilterationInput = () => {
+  const { theme } = useThemeContext();
+  return (
+    <>
+      <form className={cn("relative", Content)}>
+        <div className="absolute top-7 left-[30.6pc]">
+          <Search color="#a3a3a3" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search for products"
+          className={cn(
+            "  rounded-xl focus:outline-0 font-bold w-full py-3 px-10",
+            theme == "light" ? "bg-[#d1d1d1]" : "bg-black border"
+          )}
+        />
+      </form>
+    </>
+  );
+};
+
+export default FilterationInput;
---
+++ src/Features/Products/Components/FeatureProducts.tsx
@@ -0,0 +1,15 @@
+import { Content } from "../../../Style/GlobalStyle";
+
+const FeatureProducts = () => {
+  return (
+    <>
+      <div className={Content}>
+        <div>
+          <h1 className="text-3xl font-bold">Feature Products</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FeatureProducts;

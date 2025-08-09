00209629ca2669d5fa6187c06e65edf688414ad9
moshaosama <mohamedOSFekry@gmail.com>
2025-08-10 01:50:01 +0300

    feat: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import "./App.css";
+import { ThemeProvider } from "./Context/useTheme";
+import FilterationInput from "./Features/Filteration/Components/FilterationInput";
+import FeatureProducts from "./Features/Products/Components/FeatureProducts";
+import Navbar from "./Layouts/Navbar";
+import ThemeFactory from "./Utils/ThemeFactory";
+
+const NavbarTheme = ThemeFactory(Navbar);
+const FilterationInputTheme = ThemeFactory(FilterationInput);
+const FeatureProductsTheme = ThemeFactory(FeatureProducts);
 
 function App() {
   return (
     <>
-      <div>
-        <h1>asda</h1>
-      </div>
+      <ThemeProvider>
+        <NavbarTheme />
+        <FilterationInputTheme />
+        <FeatureProductsTheme />
+      </ThemeProvider>
     </>
   );
 }

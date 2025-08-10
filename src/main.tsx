1678535269be982dff6a9cb1f698f92ff6fb1714
moshaosama <mohamedOSFekry@gmail.com>
2025-08-10 15:08:29 +0300

    chore: update main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { Store } from "./Store/store.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import "./i18n.ts";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(

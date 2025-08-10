892e996b939e225b88d38030e485446c8bfa86f6
moshaosama <mohamedOSFekry@gmail.com>
2025-08-10 15:12:38 +0300

    feat: update useChangeLanguages hook

--- src/Hooks/useChangeLanguages.ts
+++ src/Hooks/useChangeLanguages.ts
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const useChangeLanguages = () => {
   const { t, i18n } = useTranslation();
+  const [Lang, setLang] = useState("en");
 
   const ChangeLanuage = (lang: string) => {
     i18n.changeLanguage(lang);
     document.dir = lang === "ar" ? "rtl" : "ltr";
+    setLang(lang);
   };
 
-  return { t, ChangeLanuage };
+  return { t, ChangeLanuage, Lang };
 };
 
 export default useChangeLanguages;

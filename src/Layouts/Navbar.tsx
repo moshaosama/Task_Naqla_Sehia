ca08ec6f7b6503712f3f44bcd21ac7ee4b403322
moshaosama <mohamedOSFekry@gmail.com>
2025-08-10 15:08:29 +0300

    feat: update Navbar component

--- src/Layouts/Navbar.tsx
+++ src/Layouts/Navbar.tsx
@@ -2,20 +2,38 @@ import { Moon, ShoppingCart, Sun } from "lucide-react";
 import { Container } from "../Style/GlobalStyle";
 import { useThemeContext } from "../Context/useTheme";
 import { cn } from "../lib/utils";
+import useChangeLanguages from "../Hooks/useChangeLanguages";
 const Navbar = () => {
   const { toggleTheme, theme } = useThemeContext();
+  const { ChangeLanuage, t } = useChangeLanguages();
+
   return (
     <>
       <div className="py-4">
         <div className={cn(Container, "flex justify-between")}>
           <div className="flex items-center gap-3">
             <ShoppingCart />
-            <h1 className="text-xl font-bold">Shop Online</h1>
+            <h1 className="text-xl font-bold">{t("BrandName")}</h1>
           </div>
 
           <div className="flex items-center gap-10">
             <div onClick={toggleTheme} className="cursor-pointer">
-              {theme === "light" ? <Sun /> : <Moon />}
+              {theme === "light" ? <Moon /> : <Sun />}
+            </div>
+
+            <div>
+              <button
+                onClick={() => ChangeLanuage("en")}
+                className="border py-1 px-5 rounded-2xl hover:bg-[gray] transition-all duration-700 cursor-pointer"
+              >
+                en
+              </button>
+              <button
+                onClick={() => ChangeLanuage("ar")}
+                className="border py-1 px-5 hover:bg-[gray] transition-all duration-700 rounded-2xl cursor-pointer"
+              >
+                ar
+              </button>
             </div>
 
             <div>

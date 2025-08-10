2bd11f300f7e05d425eab5732e1bfeb6a4cd477b
moshaosama <mohamedOSFekry@gmail.com>
2025-08-10 03:43:54 +0300

    feat: enhance FeatureProducts component

--- src/Features/Products/Components/FeatureProducts.tsx
+++ src/Features/Products/Components/FeatureProducts.tsx
@@ -1,8 +1,21 @@
+import { useThemeContext } from "../../../Context/useTheme";
 import { Content } from "../../../Style/GlobalStyle";
 import useGetProducts from "../Hooks/useGetProducts";
+import { ClockLoader } from "react-spinners";
 
 const FeatureProducts = () => {
-  const { product } = useGetProducts();
+  const { filteredProducts, isLoading } = useGetProducts();
+  const { theme } = useThemeContext();
+
+  if (filteredProducts?.length === 0 && !isLoading) {
+    return (
+      <>
+        <div className="flex justify-center">
+          <h1 className="text-4xl font-bold text-red-700">No products</h1>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
@@ -12,22 +25,36 @@ const FeatureProducts = () => {
         </div>
 
         <div className="grid grid-cols-4 gap-4 my-10">
-          {product.map((product) => (
-            <div className="border-[0.5px] border-[gray] cursor-pointer  hover:shadow-xl hover:shadow-[gray] transition-all duration-300 p-3 rounded-xl flex flex-col gap-2">
-              <div>
-                <img
-                  src={product.image}
-                  alt="productImage.png"
-                  className="w-full h-40 object-contain py-5"
-                />
-              </div>
-
-              <div className="flex flex-col gap-2">
-                <h1 className="font-bold">{product.title}</h1>
-                <p className="text-[gray] font-semibold">{product.category}</p>
-              </div>
+          {isLoading ? (
+            <div className="py-[15.9pc] px-[30pc]">
+              <ClockLoader
+                color={theme === "light" ? "black" : "white"}
+                size={100}
+              />
             </div>
-          ))}
+          ) : (
+            filteredProducts.map((product) => (
+              <div
+                key={product.id}
+                className="border-[0.5px] border-[gray] cursor-pointer  hover:shadow-xl hover:shadow-[gray] transition-all duration-300 p-3 rounded-xl flex flex-col gap-2"
+              >
+                <div>
+                  <img
+                    src={product.image}
+                    alt="productImage.png"
+                    className="w-full h-40 object-contain py-5"
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <h1 className="font-bold">{product.title}</h1>
+                  <p className="text-[gray] font-semibold">
+                    {product.category}
+                  </p>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </>

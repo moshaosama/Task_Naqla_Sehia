dd137fcd40fe598857f134a0ed911a1231f8d39a
moshaosama <mohamedOSFekry@gmail.com>
2025-08-10 03:43:55 +0300

    feat: update useGetProducts hook

--- src/Features/Products/Hooks/useGetProducts.ts
+++ src/Features/Products/Hooks/useGetProducts.ts
@@ -1,15 +1,32 @@
+import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../../../Store/store";
-import { useEffect } from "react";
 import { FetchProducts } from "../Actions/ProdutActions";
+import { useFilteraionContext } from "../../../Context/useFilteration";
 
 const useGetProducts = () => {
   const { product } = useSelector((state: RootState) => state.Products);
-  const Dispatch = useDispatch<AppDispatch>();
+  const { Category } = useFilteraionContext();
+  const dispatch = useDispatch<AppDispatch>();
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    Dispatch(FetchProducts());
-  }, []);
-  return { product };
+    const handleFetchProduct = async () => {
+      setIsLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setIsLoading(false);
+      await dispatch(FetchProducts());
+    };
+    handleFetchProduct();
+  }, [dispatch]);
+
+  const filteredProducts = useMemo(() => {
+    if (!product || product.length === 0) return [];
+    if (Category === "all" || Category === "") return product;
+    return product.filter((p) => p.category === Category);
+  }, [product, Category]);
+
+  return { filteredProducts, Category, isLoading };
 };
 
 export default useGetProducts;
